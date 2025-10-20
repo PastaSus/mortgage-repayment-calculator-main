1c8484c23c49e3b82e7fedf85af28dab3ae58a00
Aaron Cerezo <aarongtxd@gmail.com>
2025-10-20 21:53:43 +0800

    feat: custom checked state for radio btn

--- mortgage-repay-calc/src/components/RadioGroup/MortgageType.jsx
+++ mortgage-repay-calc/src/components/RadioGroup/MortgageType.jsx
@@ -3,23 +3,23 @@ function MortgageType() {
     <fieldset className="m-0 mt-4 border-none p-0">
       <legend className="text-[var(--Slate-700)]">Mortgage Type</legend>
       <div className="mt-2 grid gap-2">
-        <label className="flex cursor-pointer items-center gap-3 rounded-sm border border-[var(--Slate-500)] px-4 py-2 has-[input:checked]:border-[var(--Lime)] has-[input:checked]:bg-[var(--Lime)]/10">
+        <label className="radio__control grid cursor-pointer grid-cols-[1em_auto] gap-[0.1em] rounded-sm border border-[var(--Slate-500)] px-4 py-2 has-[input:checked]:border-[var(--Lime)] has-[input:checked]:bg-[var(--Lime)]/10">
           <input
             type="radio"
             value="Repayment"
             name="mortgageType"
-            className="relative m-0 h-5 w-5 cursor-pointer rounded-full border-1 border-[var(--Slate-500)] bg-white accent-[var(--Lime)] checked:border-[6px] checked:border-[var(--White)] checked:bg-white"
+            className="relative m-0 grid h-[0.7em] w-[0.7em] translate-y-[0.075em] cursor-pointer appearance-none place-content-center rounded-full border-1 border-[var(--Slate-700)] bg-white text-[currentColor] transition-transform duration-120 before:h-[0.5em] before:w-[0.5em] before:scale-0 before:rounded-full before:shadow-[inset_1em_1em_var(--Lime)] before:ease-in-out checked:border-[var(--Lime)] checked:bg-white checked:before:scale-100"
           />
           <span className="text-lg font-medium text-[var(--Slate-900)]">
             Repayment
           </span>
         </label>
-        <label className="flex cursor-pointer items-center gap-3 rounded-sm border border-[var(--Slate-500)] px-4 py-2 has-[input:checked]:border-[var(--Lime)] has-[input:checked]:bg-[var(--Lime)]/10">
+        <label className="radio__control grid cursor-pointer grid-cols-[1em_auto] gap-[0.1em] rounded-sm border border-[var(--Slate-500)] px-4 py-2 has-[input:checked]:border-[var(--Lime)] has-[input:checked]:bg-[var(--Lime)]/10">
           <input
             type="radio"
             value="Interest Only"
             name="mortgageType"
-            className="relative m-0 h-5 w-5 cursor-pointer rounded-full border-1 border-solid border-[var(--Slate-700)] bg-white accent-[var(--Lime)] checked:border-[6px] checked:border-[var(--White)] checked:bg-white"
+            className="relative m-0 grid h-[0.7em] w-[0.7em] translate-y-[0.075em] cursor-pointer appearance-none place-content-center rounded-full border-1 border-[var(--Slate-700)] bg-white text-[currentColor] transition-transform duration-120 before:h-[0.5em] before:w-[0.5em] before:scale-0 before:rounded-full before:shadow-[inset_1em_1em_var(--Lime)] before:ease-in-out checked:border-[var(--Lime)] checked:bg-white checked:before:scale-100"
           />
           <span className="text-lg font-medium text-[var(--Slate-900)]">
             Interest Only
